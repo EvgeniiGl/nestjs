e7f1b8864ef20bfe622cf17e157da794aae3cce6
ev_geniy <geka_nkz@mail.ru>
2023-06-29 18:34:47 +0700

    008-nestjs-db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { BookModule } from './book/book.module';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {BookModule} from './book/book.module';
+import {MongooseModule} from '@nestjs/mongoose';
+import {ConfigModule} from "@nestjs/config";
+import { BookController } from './book/book.controller';
+import { BooksController } from './book/books.controller';
 
 @Module({
-  imports: [BookModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forRoot(process.env.ME_CONFIG_MONGODB_CONNECT),
+        BookModule
+    ],
+    controllers: [AppController, BookController, BooksController],
+    providers: [AppService],
 })
+
 export class AppModule {}
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,24 +1,39 @@
 import {Injectable} from '@nestjs/common';
-import {Book} from "./Models/Book";
+import {Book, BookDocument} from "./Models/Book";
+import {InjectConnection, InjectModel} from "@nestjs/mongoose";
+import {Connection, Model} from "mongoose";
+import {CreateBookDto} from "./interfaces/dto/create.book.dto";
 
 @Injectable()
 export class BookService {
 
-    private readonly books: Book[] = [
-        {
-            authors: 'string;',
-            description: 'string;',
-            favorite: 'string;',
-            fileBook: 'string;',
-            fileCover: 'string;',
-            fileName: 'string;',
-            title: 'string;',
-        }
-    ];
+    constructor(
+        @InjectModel(Book.name) private BookModel: Model<BookDocument>,
+        @InjectConnection() private connection: Connection) {
+    }
 
-    findAll(): Promise<Book[]> {
-        return new Promise((resolve, reject) => {
-            resolve(this.books);
-        });
+    public fetchAll(): Promise<BookDocument[]> {
+        const books = this.BookModel.find({});
+
+        return books;
+    }
+
+    public create(data: CreateBookDto): Promise<BookDocument> {
+        const book = new this.BookModel(data);
+
+        return book.save();
+    }
+
+    public async update(id: string, data: CreateBookDto): Promise<BookDocument> {
+        let updatedBook = await this.BookModel.findByIdAndUpdate(id, data)
+
+        return this.BookModel.findById(id);
+    }
+
+    public async delete(id: string): Promise<true> {
+        const book = this.BookModel.findById(id);
+        await this.BookModel.deleteOne(book)
+
+        return true;
     }
 }
